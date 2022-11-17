98b63c70924ccdec299636c1643bf0d6040f90d7
androidtut <techkrishna2323@gmail.com>
2022-11-17 19:22:42 +0545

    android ui design part 2 completed

---
+++ app/src/main/java/com/example/uidesigntutorial/UIDesign2.java
@@ -0,0 +1,14 @@
+package com.example.uidesigntutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UIDesign2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uidesign2);
+    }
+}
